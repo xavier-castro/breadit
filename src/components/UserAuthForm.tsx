2df7ecfc9a3ee1c659d57949cf56cfbf99b93e33
xavier-castro <xavier_castro@icloud.com>
2023-06-21 18:13:17 -0500

    Add google auth button

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,13 +1,38 @@
+"use client";
+
 import { cn } from "@/lib/utils";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Button } from "./ui/Button";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      // Toast notification
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn("flex justify-center", className)} {...props}>
-      <Button>Google</Button>
+      <Button
+        size="sm"
+        className="w-full"
+        isLoading={isLoading}
+        onClick={loginWithGoogle}
+      >
+        <Icons.google className="h-6 w-6 mr-2" />
+        Google
+      </Button>
     </div>
   );
 };
