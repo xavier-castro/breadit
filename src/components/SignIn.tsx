ff89d64a2e28b35dd1e2d8caa07c6434736272d7
xavier-castro <xavier_castro@icloud.com>
2023-06-21 18:08:56 -0500

    Create UserAuthForm

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 import { Icons } from "./Icons";
+import Link from "next/link";
+import UserAuthForm from "./UserAuthForm";
 
 interface SignInProps {}
 
@@ -11,6 +13,19 @@ const SignIn: FC<SignInProps> = ({}) => {
         <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
         <p className="text-sm max-w-xs mx-auto">
           By continiung, you agree to our User Agreement and Privacy Policy.
+        </p>
+
+        {/* Sign in form */}
+        <UserAuthForm />
+
+        <p className="px-8 text-center text-sm text-zinc-700">
+          New to Breadit?{" "}
+          <Link
+            href="/sign-up"
+            className="hover:text-zinc-800 text-sm underline underline-offset-4"
+          >
+            Sign Up
+          </Link>
         </p>
       </div>
     </div>
---
+++ src/components/UserAuthForm.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+import { FC } from "react";
+import { Button } from "./ui/Button";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button>Google</Button>
+    </div>
+  );
+};
+
+export default UserAuthForm;
